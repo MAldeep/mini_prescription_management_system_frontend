6c9d5e1ebf20e26b3dbab884921357c18e6b23db
MAldeep <m7mdeldeeeep@gmail.com>
2026-08-06 21:49:41 +0300

    custom hook for handling prescriptions

---
+++ src/hooks/usePrescriptions.ts
@@ -0,0 +1,34 @@
+import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
+import { prescriptionServices } from "../services/prescriptionService";
+import type { Prescription } from "../types/prescription";
+export const QUERY_KEYS = {
+  PRESCRIPTIONS: ["prescriptions"] as const,
+};
+export const usePrescriptions = () => {
+  const querClient = useQueryClient();
+
+  // query
+  const prescriptionQuery = useQuery({
+    queryKey: QUERY_KEYS.PRESCRIPTIONS,
+    queryFn: prescriptionServices.getPrescriptions,
+    staleTime: 5 * 60 * 1000,
+  });
+  const totalPrescriptionsCount = prescriptionQuery.data?.length ?? 0;
+  // mutation
+  const createPrescriptionMutation = useMutation({
+    mutationFn: (dto: Prescription) =>
+      prescriptionServices.createPrescription(dto),
+    onSuccess: () => {
+      querClient.invalidateQueries({ queryKey: QUERY_KEYS.PRESCRIPTIONS });
+    },
+  });
+  return {
+    prescriptions: prescriptionQuery.data,
+    isLoading: prescriptionQuery.isPending,
+    isError: prescriptionQuery.isError,
+    error: prescriptionQuery.error,
+    totalPrescriptionsCount,
+    createPrescription: createPrescriptionMutation.mutate,
+    isCreating: createPrescriptionMutation.isPending,
+  };
+};
